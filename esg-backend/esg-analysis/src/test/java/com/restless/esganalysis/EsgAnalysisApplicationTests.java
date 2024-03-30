3ba9bbd044baa78674146f979dad52019c7644c5
VasilKamburov <tulevskiv@gmail.com>
2024-03-30 13:42:17 +0200

    Initial back-end commit

---
+++ esg-backend/esg-analysis/src/test/java/com/restless/esganalysis/EsgAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restless.esganalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
