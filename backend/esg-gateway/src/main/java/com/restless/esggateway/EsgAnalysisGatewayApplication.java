f9d0cbec367a5e87bd4f8a4d11aeb9640e03ed91
VasilKamburov <tulevskiv@gmail.com>
2024-03-30 13:48:01 +0200

    Initial back-end commit

---
+++ backend/esg-gateway/src/main/java/com/restless/esggateway/EsgAnalysisGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.restless.esggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgAnalysisGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgAnalysisGatewayApplication.class, args);
+	}
+
+}
---
+++ backend/esg-gateway/src/test/java/com/restless/esggateway/EsgAnalysisGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restless.esggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgAnalysisGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
